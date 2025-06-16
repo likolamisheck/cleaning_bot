7052f6a57add8c9ec31781f056782ff9389023f6
Likola Misheck <likolamisheck17@gmail.com>
2025-06-16 11:55:47 -0400

    Final production version

--- sweepingrota.py
+++ sweepingrota.py
@@ -128,7 +128,3 @@ def generate_qrcode():
     qr.save(img_io, 'PNG')
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png')
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
